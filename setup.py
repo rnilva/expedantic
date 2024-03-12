698eab858b03b443072e3196c5e519e02eb52406
rnilva <rnilva849@gmail.com>
2024-03-12 17:23:37 +0900

    Add python requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="expedantic",
     version="0.1",
     packages=find_packages(),
+    python_requires=">=3.10",
     install_requires=[
         "pydantic >= 2.6.3",
         "pydantic_yaml >= 1.2.1",
