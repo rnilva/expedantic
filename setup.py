a7c6e488e2f149578cab25aec7edf6bdff79397a
rnilva <rnilva849@gmail.com>
2024-03-12 18:06:13 +0900

    Set version 0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="expedantic",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[
