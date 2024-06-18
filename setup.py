b181301f21aede6e6ca83909e20c3208457f2acb
rnilva <rnilva849@gmail.com>
2024-06-18 18:30:25 +0900

    Update to version 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="expedantic",
-    version="0.1.6",
+    version="0.1.7",
     packages=["expedantic", "ccorp.ruamel.yaml.include"],
     package_dir={
         # "expedantic": "src",
