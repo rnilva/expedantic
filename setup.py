9d01810dc3e8d8b0e70d7339c036e084f80a3f84
rnilva <rnilva849@gmail.com>
2024-12-11 17:33:27 +0900

    🔖 0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="expedantic",
-    version="0.1.7",
+    version="0.1.8",
     packages=["expedantic", "ccorp.ruamel.yaml.include"],
     package_dir={
         # "expedantic": "src",
